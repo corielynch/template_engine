52067b71d7513ff0b3a309aeb0645514b7e77304
Corie Lynch <Lynchc17@gmail.com>
2020-06-08 16:03:13 -0700

    Rendered user input to html

--- app.js
+++ app.js
@@ -12,49 +12,84 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employee's name?",
+      name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the employee's role?",
+        name: "role"
+    },
+    {
+      type: "input",
+      message: "What is the employee's ID number",
+      name: "ID"
+    },
+    {
+        type: "input",
+        message: "What is the employee's email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the employee's office number?",
+        name: "office"
+    },
+    {
+        type: "input",
+        message: "What is the employee's github username?",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "What is the employee's school?",
+        name: "school"
+    },
+]).then(function(data) {
+    
 //Creates objects for each team member 
-const Jared = new Manager("Jared", 1, "jared@fakemail.com", 1);
-console.log(Jared);
+// const Jared = new Manager("Jared", 1, "jared@fakemail.com", 1);
+// console.log(Jared);
 
-const Alec = new Engineer("Alec", 2, "alec@fakemail.com", "ibealec");
-console.log(Alec);
+// const Alec = new Engineer("Alec", 2, "alec@fakemail.com", "ibealec");
+// console.log(Alec);
 
-const Tammer = new Engineer("Tammer", 3, "tammer@fakemail.com", "tammerg");
-console.log(Tammer);
+// const Tammer = new Engineer("Tammer", 3, "tammer@fakemail.com", "tammerg");
+// console.log(Tammer);
 
-const Christian = new Engineer("Christian", 4, "christian@fakemail.com", "ceckenrode");
-console.log(Christian);
+// const Christian = new Engineer("Christian", 4, "christian@fakemail.com", "ceckenrode");
+// console.log(Christian);
 
-const John = new Intern("John", 5, "john@fakemail.com", "2University");
-console.log(John);
+// const John = new Intern("John", 5, "john@fakemail.com", "2University");
+// console.log(John);
+var employee;
+if (data.role === "Manager"){
+    employee = new Manager(data.name, data.role, data.ID, data.email, data.office);
+}
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+else if(data.role === "Intern"){
+    employee = new Intern(data.name, data.role, data.ID, data.email, data.school);
+}
 
-render([Jared, Alec, Tammer, Christian, John]);
-fs.writeFile("output/team.html", outputPath, function(err) {
+else if(data.role === "Engineer"){
+    employee = new Engineer(data.name, data.role, data.ID, data.email, data.github);
+}
 
+else {
+    employee = new Employee(data.name, data.role, data.ID, data.email, data.github);
+}
+
+
+var html = render([employee]);
+fs.writeFile(outputPath, html, function(err) {
+    
     if (err) {
       return console.log(err);
     }
-  
     console.log("Success!");
   
   });
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+});
